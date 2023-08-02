a11c65483442729780bab25413e14e5bdd3ba2ca
Breno Lima <85833737+BrenoLim4@users.noreply.github.com>
2023-08-02 17:18:59 -0300

    first commit

---
+++ src/main/java/edu/br/empenhoapi/EmpenhoApiApplication.java
@@ -0,0 +1,13 @@
+package edu.br.empenhoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpenhoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpenhoApiApplication.class, args);
+	}
+
+}
